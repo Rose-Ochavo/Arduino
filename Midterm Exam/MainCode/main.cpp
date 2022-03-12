28cad5420310b6d76fae4a0c47d70056b91ac30c
MyGithub@04 <52346651+alrose102318@users.noreply.github.com>
2022-03-13 02:18:42 +0800

    Update main.cpp

--- Midterm Exam/MainCode/main.cpp
+++ Midterm Exam/MainCode/main.cpp
@@ -20,7 +20,6 @@ void setup() {
 	pinMode(R2, INPUT);
 	pinMode(RMost, INPUT);
 
-
 	pinMode(ena, OUTPUT);
 	pinMode(in1, OUTPUT);
 	pinMode(in2, OUTPUT);
@@ -33,31 +32,34 @@ void loop() {
 	if(digitalRead(Mid == 0)) {//ForwardMaxSpeed
 		Maxforward();
 	}
-
 	if((digitalRead(L1) == 1) && (digitalRead(R2) == 1)) {//ForwardRun
 		Forward();
 	}
 	if((digitalRead(L1) == 0) && (digitalRead(R2) == 1)) {//TurnRight
 		TRight();
 	}
-	if((digitalRead(LMost) == 0) && (digitalRead(R2) == 1)){//HardRight
+	if((digitalRead(LMost) == 0) && (digitalRead(R2) == 1)) {//HardRight
 		HRight();
 	}
-	if((digitalRead(L1) == 1) && (digitalRead(R2) == 0)){//TurnLeft
+	if((digitalRead(L1) == 1) && (digitalRead(R2) == 0)) {//TurnLeft
 		TLeft();
 	}
-	if((digitalRead(L1) == 1) && (digitalRead(RMost) == 0))//HardLeft
+	if((digitalRead(L1) == 1) && (digitalRead(RMost) == 0)) {//HardLeft
 		HLeft();
+	}
+	if((digitalRead(LMost == 0)) && (digitalRead(L1 == 0)) && (digitalRead(Mid == 0)) && (digitalRead(R2 == 0)) && (digitalRead(RMost == 0))) {//ForwardMaxSpeed
+		Stop();
+	}
 }
 
 void Maxforward(){
 	digitalWrite(in1,HIGH);
-  	digitalWrite(in2,LOW);
-  	analogWrite(ena, 255);
+	digitalWrite(in2,LOW);
+	analogWrite(ena, 255);
   
  	digitalWrite(in3,HIGH);
-  	digitalWrite(in4,LOW);
-  	analogWrite(enb, 255);
+	digitalWrite(in4,LOW);
+	analogWrite(enb, 255);
 }
 void Forward(){
 	digitalWrite(in1,HIGH);
@@ -75,81 +77,22 @@ void TRight(){
 }
 void HRight(){
 	digitalWrite(in3,HIGH);
-  	digitalWrite(in4,LOW);
-  	analogWrite(enb, 255);
+	digitalWrite(in4,LOW);
+	analogWrite(enb, 255);
 }
 void TLeft(){
 	digitalWrite(in1,HIGH);
-  	digitalWrite(in2,LOW);
-  	analogWrite(ena, 127);
+	digitalWrite(in2,LOW);
+	analogWrite(ena, 127);
 }
 void HLeft(){
 	digitalWrite(in1,HIGH);
  	digitalWrite(in2,LOW);
-  	analogWrite(ena, 255);
+	analogWrite(ena, 255);
 }
-
-// END OF CODE
-
-
-
-	//MOTOR_A CLOCKWISE MAX SPEED
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(ena, 255);
-  
-  //MOTOR_B CLOCKWISE MAX SPEED
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  analogWrite(enb, 255);
-  delay(2000);
-  
-  //STOP
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,LOW);
-  delay(2000);
-
-  // //MOTOR_A COUNTERCLOCKWISE MAX SPEED
-  // digitalWrite(in1,LOW);
-  // digitalWrite(in2,HIGH);
-  // analogWrite(ena, 255);
-
-  // //MOTOR_B COUNTERCLOCKWISE MAX SPEED
-  // digitalWrite(in3,LOW);
-  // digitalWrite(in4,HIGH);
-  // analogWrite(enb, 255);
- 
-  // delay(2000);
-
-  //MOTOR_A CLOCKWISE HALF SPEED 
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(ena, 127);
-  
-  //MOTOR_B CLOCKWISE HALF SPEED
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  analogWrite(enb, 127);
-  delay(2000);
-
-  // //MOTOR_A COUNTERCLOCKWISE HALF SPEED
-  // digitalWrite(in1,LOW);
-  // digitalWrite(in2,HIGH);
-  // analogWrite(ena, 127);
-
-  // //MOTOR_B COUNTERCLOCKWISE HALF SPEED
-  // digitalWrite(in3,LOW);
-  // digitalWrite(in4,HIGH);
-  // analogWrite(enb, 127);
- 
-  // delay(2000);
-
-
-void backwardRun() {
-  digitalWrite(in1 , HIGH);
-  digitalWrite(in2 , LOW);
-  digitalWrite(in3 , HIGH);
-  digitalWrite(in4 , LOW);
+void Stop(){
+	digitalWrite(in1,LOW);
+	digitalWrite(in2,LOW);
+	digitalWrite(in3,LOW);
+	digitalWrite(in4,LOW);
 }
