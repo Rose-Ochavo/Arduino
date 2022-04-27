54506825eb74a0acd08e9201b1c0086e162a6067
MyGithub@04 <52346651+alrose102318@users.noreply.github.com>
2022-04-28 00:31:14 +0800

    Create Scratch Code.ino

---
+++ FINALS/Exercise #2/Scratch Code.ino
@@ -0,0 +1,34 @@
+int led_pin = 6;
+int P_button = 3;
+
+void setup(){
+  pinMode(led_pin, OUTPUT);
+  pinMode(P_button, INPUT);
+}
+
+void loop(){
+  int state = 0;
+
+  if (digitalRead(P_button) == HIGH){
+    for(int i = state; i < 255; i++){
+      analogWrite(led_pin, i);
+      state++;
+      delay(5);
+      break;
+    }
+  }
+
+  if (digitalRead(P_button) == LOW){
+    for(int i = state; i > 0; i--){
+      analogWrite(led_pin, i);
+      state--;
+      delay(5);
+      break;      
+    }
+  }
+}
+
+pinMode(3, INPUT);
+if (digitalRead(P_button) == LOW){
+while(1);
+}
