13831651666f43675fc34e9359acfd621a140c8c
@04 <52346651+A50426224@users.noreply.github.com>
2022-05-03 01:58:51 +0800

    Update servo motor with blink (OK).ino

--- FINALS/Final Project - Robot StartupInitialization Simulation/servo motor with blink (OK).ino
+++ FINALS/Final Project - Robot StartupInitialization Simulation/servo motor with blink (OK).ino
@@ -4,6 +4,10 @@ int pos = 0;
 
 Servo servo_9;
 
+const unsigned long BLINK_DELAY = 100 ;  
+unsigned long blink_time = 0 ;
+bool led_on = false ;
+
 void setup()
 {
   servo_9.attach(9, 500, 2500);
@@ -12,22 +16,42 @@ void setup()
 
 void loop()
 {
-  blink();
- 
+  start_blink();
   for (pos = 0; pos <= 180; pos++) {  
     servo_9.write(pos);
     
     delay(15); 
+    blink(); 
   }
+  stop_blink();
+
+  delay(2000); 
+
+  start_blink();
   for (pos = 180; pos >= 0; pos--) {    
     servo_9.write(pos);
     
     delay(15); 
+    blink();
   }
+  stop_blink(); 
 }
-void blink() {
+
+void start_blink() {
   digitalWrite(13, HIGH);
-  delay(100); 
-  digitalWrite(13, LOW);
-  delay(100);
+  led_on = true ;
+  blink_time = millis() + BLINK_DELAY ;
+}
+
+void stop_blink() {
+  digitalWrite(13,LOW);
+  led_on = false ;
+}
+
+void blink() {
+  if ( millis() > blink_time ) {
+    led_on = !led_on ;
+    digitalWrite(13, led_on ? HIGH : LOW);
+    blink_time = millis() + BLINK_DELAY ;
+  }
 }
