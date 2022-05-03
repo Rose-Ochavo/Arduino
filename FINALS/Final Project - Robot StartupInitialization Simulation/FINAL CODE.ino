101fb2df15ad7d760f82ed2c0ddb3d4f641660d0
@04 <52346651+A50426224@users.noreply.github.com>
2022-05-03 20:46:55 +0800

    Add files via upload

---
+++ FINALS/Final Project - Robot StartupInitialization Simulation/FINAL CODE.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+Servo servoPin;
+
+const byte trigger_pin = A1;
+const byte echo_pin = A2;
+
+long distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(echo_pin, INPUT);
+  pinMode(trigger_pin, OUTPUT); 
+
+  servoPin.attach(9, 500, 2500);
+  // servoPin.write(90);
+  delay(1000);  
+}
+
+void loop() {
+  Serial.println(distance);
+
+  digitalWrite(trigger_pin, HIGH);
+  delay(10);
+  digitalWrite(trigger_pin, LOW);
+
+  distance = pulseIn(echo_pin, HIGH);
+  delay(100);
+
+  if(distance <= 300){
+    servoPin.write(180);    
+    delay(1000);
+
+    servoPin.write(0);
+    delay(1000);
+
+    servoPin.write(90);
+    delay(1000);
+  }
+  else{
+    servoPin.write(90);
+  }
+}
