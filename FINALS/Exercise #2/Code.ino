30e4f3491a041c9f04769d1348c7759a302ba134
@04 <52346651+A50426224@users.noreply.github.com>
2022-05-03 11:48:25 +0800

    Update Code.ino

--- FINALS/Exercise #2/Code.ino
+++ FINALS/Exercise #2/Code.ino
@@ -12,7 +12,7 @@ void loop()
 {
   if (digitalRead(Pbutton) == HIGH) {
     if (state != 255) {
-      state+=5;
+      state+=3;
       delay(30); 
     }
     analogWrite(led, state);
@@ -20,7 +20,7 @@ void loop()
 
   if (digitalRead(Pbutton) == LOW) {
     if (state != 0) {
-      state-=5;
+      state-=3;
       delay(30);
     }
     analogWrite(led, state);    
